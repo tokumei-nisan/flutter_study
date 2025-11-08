e6e3e8cc51f6cc9cd81dcb09fc406b70228ae33e
tokumei-nisan <tokumei.nisan@gmail.com>
2025-11-08 17:48:49 +0900

    Raw widget

--- column_row_widget/lib/main.dart
+++ column_row_widget/lib/main.dart
@@ -17,21 +17,25 @@ class MyApp extends StatelessWidget {
               width: double.infinity,
               height: 60,
               color: Colors.grey[200],
-              child: Column(
+              child: Row(
+                // 横に並べる
                 children: <Widget>[
-                  Text('first line'),
-                  Text('second line'),
+                  Container(color: Colors.red, child: Text('first')),
+                  Container(color: Colors.blue, child: Text('second')),
+                  Container(color: Colors.green, child: Text('third')),
                 ],
               ),
             ),
             Container(
               width: double.infinity,
-              height: 70,
-              child: Column(
+              height: 60,
+              child: Row(
+                // 中央寄せ
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text('***'),
-                  Text('中央寄せ'),
+                  Container(color: Colors.red, child: Text('***')),
+                  Container(color: Colors.blue, child: Text('中央寄せ')),
+                  Container(color: Colors.green, child: Text('---')),
                 ],
               ),
             ),
@@ -39,22 +43,26 @@ class MyApp extends StatelessWidget {
               width: double.infinity,
               height: 60,
               color: Colors.grey[200],
-              child: Column(
+              child: Row(
+                // 右寄せ
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
-                  Text('***'),
-                  Text('下寄せ'),
+                  Container(color: Colors.red, child: Text('***')),
+                  Container(color: Colors.blue, child: Text('右寄せ')),
+                  Container(color: Colors.green, child: Text('---')),
                 ],
               ),
             ),
             Container(
               width: double.infinity,
               height: 60,
-              child: Column(
+              child: Row(
+                // 均等配置
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Text('***'),
-                  Text('均等に配置'),
+                  Container(color: Colors.red, child: Text('***')),
+                  Container(color: Colors.blue, child: Text('均等配置')),
+                  Container(color: Colors.green, child: Text('---')),
                 ],
               ),
             ),
@@ -62,22 +70,26 @@ class MyApp extends StatelessWidget {
               width: double.infinity,
               height: 60,
               color: Colors.grey[200],
-              child: Column(
+              child: Row(
+                // 上寄せ
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text('***'),
-                  Text('左寄せ'),
+                  Container(color: Colors.red, child: Text('***')),
+                  Container(color: Colors.blue, child: Text('上寄せ')),
+                  Container(color: Colors.green, child: Text('---')),
                 ],
               ),
             ),
             Container(
               width: double.infinity,
               height: 60,
-              child: Column(
+              child: Row(
+                // 下寄せ
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: <Widget>[
-                  Text('***'),
-                  Text('右寄せ'),
+                  Container(color: Colors.red, child: Text('***')),
+                  Container(color: Colors.blue, child: Text('下寄せ')),
+                  Container(color: Colors.green, child: Text('---')),
                 ],
               ),
             ),
