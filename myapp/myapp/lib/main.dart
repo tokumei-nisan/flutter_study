15c310675d41d893d1f2b680d6302e84411d05ba
tokumei-nisan <tokumei.nisan@gmail.com>
2025-11-08 17:35:36 +0900

    widgetプロパティでいろいろ遊んでみた

--- myapp/myapp/lib/main.dart
+++ myapp/myapp/lib/main.dart
@@ -1,36 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            // デフォルト表示
+            Text('Default'),
+            // 太さを指定
+            Text('Bold', style: TextStyle(fontWeight: FontWeight.bold)),
+            // スタイルを指定
+            Text('Italic', style: TextStyle(fontStyle: FontStyle.italic)),
+            // サイズを指定
+            Text('fontSize = 72', style: TextStyle(fontSize: 72)),
+            // 色を指定
+            Text('BlueGrey', style: TextStyle(color: Colors.blueGrey)),
+            Container(
+              width: double.infinity,
+              color: Colors.blueGrey,
+              // 表示位置を指定
+              child: Text('TextAlign.right', textAlign: TextAlign.right),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                // 枠線
+                border: Border.all(color: Colors.blue, width: 2),
+                // 角丸
+                borderRadius: BorderRadius.circular(8),
+                image: DecorationImage(
+                  image: NetworkImage('https://placehold.jp/200x100.png'),
+                ),
+              ),
+              // 内側の余白（パディング）
+              padding: EdgeInsets.all(8),
+              // 外側の余白（マージン）
+              margin: EdgeInsets.all(8),
+              width: double.infinity,
+              //color: Colors.blue,
+              child: Text('blue'),
+            ),
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
